3f2b58317cd8a2a239fe1094652cfec39d198d57
Luke Juras <lukejuras@gmail.com>
2025-05-29 20:20:08 -0600

    Uploaded test audio

---
+++ data.js
@@ -0,0 +1,16 @@
+const instruments = {
+  trumpet: {
+    octave: 0,
+    pitches: ["F3", "A3", "C4", "F4", "A4", "C5", "F5"]
+  },
+  
+  flute: {
+    octave: 1,
+    pitches: ["F4", "A5", "C5", "F5", "A5", "C6", "F6"]
+  },
+
+  baritone: {
+    octave: -1,
+    pitches: ["F2", "A2", "C3", "F3", "A3", "C4", "F4"]
+  }
+};
