44d8647f482e4ea952855cf1099f6eba13523b61
Luke Juras <lukejuras@gmail.com>
2025-06-01 07:49:04 -0600

    unison, computer gen audio

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 const instruments = {
   trumpet: {
     octave: 0,
-    pitches: ["F3", "A3", "C4", "F4", "A4", "C5", "F5"]
+    pitches: ["F3", "Gb3", "A3", "Bb3", "C4", "Db4", "F4", "Gb4", "A4", "Bb4", "C5", "Db5", "F5"]
   },
   
   flute: {
     octave: 1,
-    pitches: ["F4", "A5", "C5", "F5", "A5", "C6", "F6"]
+    pitches: ["F4", "Gb4", "A5", "Bb4", "C5", "Db5", "F5", "Gb5", "A5", "Bb4", "C6", "Db5", "F6"]
   },
 
   baritone: {
     octave: -1,
-    pitches: ["F2", "A2", "C3", "F3", "A3", "C4", "F4"]
+    pitches: ["F2", "Gb2", "A2", "Bb2", "C3", "Db3", "F3", "Gb3", "A3", "Bb3", "C4", "Db4", "F4"]
   }
 };
