c66ea211321b1cac6380fad113f48f6906344baa
Luke Juras <lukejuras@gmail.com>
2025-06-13 16:13:32 -0600

    Added group: for selection

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const instruments = {
   piccolo: {
     octave: 2,
+    group: "woodwind",
     pitches: [
       "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5", 
       "C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6", "B6", 
@@ -8,6 +9,7 @@ const instruments = {
   },
   flute: {
     octave: 1,
+    group: "woodwind",
     pitches: ["C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
   "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5", 
   "C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6", "B6", 
@@ -15,6 +17,7 @@ const instruments = {
   },
   oboe: {
     octave: 1,
+    group: "woodwind",
     pitches: [
       "Bb3", "B3", 
       "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
@@ -24,6 +27,7 @@ const instruments = {
   },
   bassoon: {
     octave: -1,
+    group: "woodwind",
     pitches: [
       "Bb1", "B1",
       "C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
@@ -33,6 +37,7 @@ const instruments = {
   },
   clarinet: {
     octave: 0,
+    group: "woodwind",
     pitches: [
       "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
       "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
@@ -40,24 +45,27 @@ const instruments = {
       "C6", "Db6", "D6", "Eb6"
     ]
   },
-  bassclarinet: {
+  bass_clarinet: {
     octave: -1,
+    group: "woodwind",
     pitches: [
       "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
       "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4"
     ]
   },
-  altosax: {
+  alto_sax: {
     octave: 0,
+    group: "woodwind",
     pitches: [
       "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
       "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
       "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5"
     ]
   },
-  tenorsax: {
+  tenor_sax: {
     octave: 0,
+    group: "woodwind",
     pitches: [
       "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
@@ -65,8 +73,9 @@ const instruments = {
       "C5", "Db5", "D5", "Eb5"
     ]
   },
-  barisax: {
+  bari_sax: {
     octave: -1,
+    group: "woodwind",
     pitches: [
       "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
@@ -75,14 +84,16 @@ const instruments = {
   },
   trumpet: {
     octave: 0,
+    group: "brass",
     pitches: [
       "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
       "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
       "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5"
     ]
   },
-  frenchhorn: {
+  french_horn: {
     octave: 0,
+    group: "brass",
     pitches: [
       "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
@@ -92,6 +103,7 @@ const instruments = {
   },
   trombone: {
     octave: -1,
+    group: "brass",
     pitches: [
       "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
@@ -100,6 +112,7 @@ const instruments = {
   },
   baritone: {
     octave: -1,
+    group: "brass",
     pitches: [
       "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
       "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
@@ -108,6 +121,7 @@ const instruments = {
   },
   tuba: {
     octave: -2,
+    group: "brass",
     pitches: [
       "E1", "F1", "Gb1", "G1", "Ab1", "A1", "Bb1", "B1",
       "C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
@@ -116,6 +130,7 @@ const instruments = {
   },
   piano: {
     octave: 0,
+    group: "",
     pitches: [
       "E1", "F1", "Gb1", "G1", "Ab1", "A1", "Bb1", "B1",
       "C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
