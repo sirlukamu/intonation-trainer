f734cb44bf358be08b1471f7134829c50e11d658
Luke Juras <lukejuras@gmail.com>
2025-07-27 23:37:01 -0600

    new audio assets, audiotest2.html

---
+++ datacopy.js
@@ -0,0 +1,112 @@
+/* For use with audiotest.html using new TonalEnergy sound files */
+
+const instruments = {
+  "piccolo": {
+    octave: 2,
+    group: "woodwind",
+    pitches: [
+      "F5", "F6", "F7"]
+  },
+  "flute": {
+    octave: 1,
+    group: "woodwind",
+    pitches: [
+      "F4", "F5", "F6"]
+  },
+  "oboe": {
+    octave: 1,
+    group: "woodwind",
+    pitches: [
+      "F4", "F5", 
+    ]
+  },
+  "bassoon": {
+    octave: -1,
+    group: "woodwind",
+    pitches: [
+      "F2", "F3", "F4"
+     ]
+  },
+  "clarinet": {
+    octave: 0,
+    group: "woodwind",
+    pitches: [
+      "F3", "F4", "F5", 
+    ]
+  },
+  "bass-clarinet": {
+    octave: -1,
+    group: "woodwind",
+    pitches: [
+      "F2", "F3", "F4"
+    ]
+  },
+  "alto-sax": {
+    octave: 0,
+    group: "woodwind",
+    pitches: [
+      "F3", "F4", "F5", 
+    ]
+  },
+  "tenor-sax": {
+    octave: 0,
+    group: "woodwind",
+    pitches: [
+      "F3", "F4", 
+    ]
+  },
+  "bari-sax": {
+    octave: -1,
+    group: "woodwind",
+    pitches: [
+      "F2", "F3", "F4", 
+    ]
+  },
+  "trumpet": {
+    octave: 0,
+    group: "brass",
+    pitches: [
+      "F3", "F4", "F5", 
+    ]
+  },
+  "french-horn": {
+    octave: 0,
+    group: "brass",
+    pitches: [
+      "F2", "F3", "F4", "F5"
+    ]
+  },
+  "trombone": {
+    octave: -1,
+    group: "brass",
+    pitches: [
+      "F2", "F3", "F4", 
+    ]
+  },
+  "baritone": {
+    octave: -1,
+    group: "brass",
+    pitches: [
+      "F2", "F3", "F4", 
+    ]
+  },
+  "tuba": {
+    octave: -2,
+    group: "brass",
+    pitches: [
+      "F1", "F2", "F3", 
+    ]
+  },
+};
+
+const allNotes = [
+      "E1", "F1", "Gb1", "G1", "Ab1", "A1", "Bb1", "B1",
+      "C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
+      "C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3", 
+      "C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4", 
+      "C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5", 
+      "C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6", "B6", 
+      "C7", "Db7", "D7", "Eb7", "E7", "F7"
+];
+
+
